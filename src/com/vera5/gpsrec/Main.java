c92c3af57490bcca447d2e365b6cbcded00a1f3d
Nick Kolev <nickkolev@yahoo.com>
2015-08-05 13:25:48 +0200

    Introduced Notification(s)

--- src/com/vera5/gpsrec/Main.java
+++ src/com/vera5/gpsrec/Main.java
@@ -1,5 +1,8 @@
 package com.vera5.gpsrec;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.DialogInterface;
@@ -19,30 +22,39 @@ import android.location.LocationManager;
 
 public class Main extends ListActivity {
 
+  private static final int NOTIFICATION_ID = 9315;
   private gpsDatabase db;
   private ListView lv;
   private dbAdapter adapter;
   private LocationManager locationManager;
   private LocationListener locationListener;
   private String provider;
+  private NotificationManager mNM;
+  private Notification notification;
+  private boolean isRecording = false;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		// Notifications
+		mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+		notification = new Notification(R.drawable.ic_launcher, "", System.currentTimeMillis());
+		// Location
 		locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 		Criteria criteria = new Criteria();
-		provider = locationManager.getBestProvider(criteria, true);
+		provider = locationManager.getBestProvider(criteria, false);
 		try {
 			Location lastGood = locationManager.getLastKnownLocation(provider);
 			if (lastGood == null)
 				Tooltip("Last known location unavailable");
 			else
-				setTitle(getString(R.string.app_name)+" @"+lastGood.getLatitude()+","+lastGood.getLongitude());
+				updateNotifiction(""+Lib.round5(lastGood.getLatitude())+","+Lib.round5(lastGood.getLongitude()));
 		} catch (Exception e) {
 			Log.e("***", e.getMessage());
 		}
 		locationListener = new MyListener();
+		// ListView
 Log.d("***", "ts: "+System.currentTimeMillis());
 		db = new gpsDatabase(this);
         Cursor curs = db.query("SELECT rowid AS _id,t1,t2,tag FROM toc ORDER BY t2 DESC");
@@ -69,6 +81,20 @@ Log.d("***", "onclick: "+id+", t1="+curs.getFloat(1)+", t2="+curs.getFloat(2));
 		lv.setAdapter(adapter);
 	}
 
+	@Override
+	public void onDestroy() {
+		mNM.cancel(NOTIFICATION_ID);
+		super.onDestroy();
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (isRecording)
+			Tooltip("Recording");
+		else
+			super.onBackPressed();
+	}
+
 	private void Delete(final Cursor curs, final long id) {
 		new AlertDialog.Builder(this)
 			.setMessage("Delete "+curs.getString(3)+"?")
@@ -92,11 +118,19 @@ Log.d("***", "Snapshot()");
 		Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
 	}
 
+	protected void updateNotifiction(String message) {
+		CharSequence text = message;
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, Main.class), 0);
+		notification.setLatestEventInfo(this, getString(R.string.app_name), text, contentIntent);
+		mNM.notify(NOTIFICATION_ID, notification);
+	}
+
 	final class MyListener implements LocationListener {
 		@Override
 		public void onLocationChanged(Location location) {
 			Log.d("***", ""+location.getLatitude()+","+location.getLongitude()+" ("+Lib.round1(location.getAccuracy())+")");
 			Tooltip(""+location.getLatitude()+","+location.getLongitude());
+			updateNotifiction(""+location.getLatitude()+","+location.getLongitude());
 		}
 		@Override
 		public void onProviderDisabled(String provider) {
